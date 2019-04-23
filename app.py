8c854a1be9e42b83588b8ddf8e6b019f7ed7ffc8
kwilli60 <karistawilliams@hotmail.com>
2019-04-23 17:57:44 -0500

    this is it
    i need help with the second part

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import datetime as dt
 
 from flask import Flask, jsonify
 #########################################################
@@ -21,12 +22,36 @@ session=Session(engine)
 #Flask Setup
 
 app=Flask(__name__)
+
+@app.route("/")
 def welcome():
     """List all available api routes."""
     return(
         f"Available Routes:<br/>"
-        f"/api/v1.0/"
+        f"/api/precipitation<br/>"
+        f"/api/stations<br/>"
+        f"/api/temperature<br/>"
+        f"/api/<start><br/>"
+        f"/api/<start>/<end>"
+
     )
+
+    @app.route("/api/precipitation")
+    def Precip():
+        """Return the data as a json"""
+        return jsonify(Measurement)
+    
+    @app.route("/api/stations")
+    def Station():
+        """Return the data for stations as json"""
+        return jsonify(Stations)
+
+    @app.route("/api/temperature")
+    def Temp():
+        """Last year temperature"""
+        last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+        last_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+        
 
 #
 
